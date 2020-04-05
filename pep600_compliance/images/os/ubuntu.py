8592d0154f7a11b4b9c4146467736609c2f69ea7
mayeut <mayeut@users.noreply.github.com>
2020-04-06 00:38:55 +0200

    f

--- pep600_compliance/images/os/ubuntu.py
+++ pep600_compliance/images/os/ubuntu.py
@@ -23,15 +23,15 @@ UBUNTU_PYTHON_PPA = [
 
 UBUNTU_PACKAGES = ['libx11-6', 'libxext6', 'libxrender1', 'libice6', 'libsm6', 'libgl1-mesa-glx', 'libglib2.0-0']
 UBUNTU_LIST = [
-    Ubuntu('ubuntu:20.04', machines=['x86_64', 'aarch64', 's390x', 'armv7l'], packages=[['python3-pip'] + UBUNTU_PACKAGES]),  # TODO 'i686', 'ppc64le'
-    Ubuntu('ubuntu:19.10', machines=['i686', 'x86_64', 'aarch64', 'ppc64le', 's390x', 'armv7l'], packages=[['python3-pip'] + UBUNTU_PACKAGES]),  # TODO 'ppc64le'
+    Ubuntu('ubuntu:20.04', machines=['x86_64', 'aarch64', 'ppc64le', 's390x', 'armv7l'], packages=[['python3-pip'] + UBUNTU_PACKAGES]),  # TODO 'i686'
+    Ubuntu('ubuntu:19.10', machines=['i686', 'x86_64', 'aarch64', 'ppc64le', 's390x', 'armv7l'], packages=[['python3-pip'] + UBUNTU_PACKAGES]),
     Ubuntu('ubuntu:19.04', machines=['i686', 'x86_64', 'aarch64', 'ppc64le', 's390x', 'armv7l'], packages=[['python3-pip'] + UBUNTU_PACKAGES]),
     Ubuntu('ubuntu:18.10', machines=['i686', 'x86_64', 'aarch64', 'ppc64le', 's390x', 'armv7l'], packages=[['python3-pip'] + UBUNTU_PACKAGES], apt_sources_update=UBUNTU_APT_OLD),
     Ubuntu('ubuntu:18.04', machines=['i686', 'x86_64', 'aarch64', 'ppc64le', 's390x', 'armv7l'], packages=[['python3-pip'] + UBUNTU_PACKAGES]),
     Ubuntu('ubuntu:17.10', machines=['i686', 'x86_64', 'aarch64', 'ppc64le', 's390x', 'armv7l'], packages=[['python3-pip'] + UBUNTU_PACKAGES], apt_sources_update=UBUNTU_APT_OLD),
     Ubuntu('ubuntu:17.04', machines=['i686', 'x86_64', 'aarch64', 'ppc64le', 's390x', 'armv7l'], packages=[['python3-pip'] + UBUNTU_PACKAGES], apt_sources_update=UBUNTU_APT_OLD),
     Ubuntu('ubuntu:16.10', machines=['i686', 'x86_64', 's390x'], packages=[['python3-pip'] + UBUNTU_PACKAGES], apt_sources_update=UBUNTU_APT_OLD),
-    Ubuntu('ubuntu:16.04', machines=['i686', 'x86_64', 'aarch64', 'ppc64le', 's390x', 'armv7l'], packages=[['python3-pip'] + UBUNTU_PACKAGES]),  # TODO 's390x'
+    Ubuntu('ubuntu:16.04', machines=['i686', 'x86_64', 'aarch64', 'ppc64le', 's390x', 'armv7l'], packages=[['python3-pip'] + UBUNTU_PACKAGES]),
     Ubuntu('ubuntu:15.10', machines=['i686', 'x86_64'], packages=[['python3-pip'] + UBUNTU_PACKAGES], apt_sources_update=UBUNTU_APT_OLD),
     Ubuntu('ubuntu:15.04', machines=['i686', 'x86_64'], packages=[['python3-pip'] + UBUNTU_PACKAGES], apt_sources_update=UBUNTU_APT_OLD),
     Ubuntu('ubuntu:14.10', machines=['x86_64'], packages=[['python3-pip'] + UBUNTU_PACKAGES], apt_sources_update=UBUNTU_APT_OLD),
