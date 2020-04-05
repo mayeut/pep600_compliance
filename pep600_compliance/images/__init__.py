62bb1cce58fc713da70ab646bfff6bb173ec9500
mayeut <mayeut@users.noreply.github.com>
2020-04-05 22:25:55 +0200

    Add clefos

--- pep600_compliance/images/__init__.py
+++ pep600_compliance/images/__init__.py
@@ -3,6 +3,7 @@ from pep600_compliance.images.os.amazonlinux import AMAZONLINUX_LIST
 from pep600_compliance.images.os.archlinux import ARCHLINUX_LIST
 from pep600_compliance.images.os.centos import CENTOS_LIST
 from pep600_compliance.images.os.clearlinux import CLEARLINUX_LIST
+from pep600_compliance.images.os.clefos import CLEFOS_LIST
 from pep600_compliance.images.os.debian import DEBIAN_LIST
 from pep600_compliance.images.os.fedora import FEDORA_LIST
 from pep600_compliance.images.os.mageia import MAGEIA_LIST
@@ -12,7 +13,7 @@ from pep600_compliance.images.os.photon import PHOTON_LIST
 from pep600_compliance.images.os.ubuntu import UBUNTU_LIST
 
 
-IMAGE_LIST = ALT_LIST + AMAZONLINUX_LIST + ARCHLINUX_LIST + CENTOS_LIST + CLEARLINUX_LIST + DEBIAN_LIST + FEDORA_LIST + MAGEIA_LIST + OPENSUSE_LIST + ORACLELINUX_LIST + PHOTON_LIST + UBUNTU_LIST
+IMAGE_LIST = ALT_LIST + AMAZONLINUX_LIST + ARCHLINUX_LIST + CENTOS_LIST + CLEARLINUX_LIST + CLEFOS_LIST + DEBIAN_LIST + FEDORA_LIST + MAGEIA_LIST + OPENSUSE_LIST + ORACLELINUX_LIST + PHOTON_LIST + UBUNTU_LIST
 
 
 def get_images(machine):
---
+++ pep600_compliance/images/os/clefos.py
@@ -0,0 +1,17 @@
+from pep600_compliance.images import base
+from pep600_compliance.images import package_manager
+
+
+class ClefOS(base.Base):
+    def __init__(self, image, pkg_manager, packages, machines):
+        name, version = image.split(':')
+        self._packages = packages
+        super().__init__(image, name, version, pkg_manager, machines)
+
+    def install_packages(self, container, machine):
+        super()._install_packages(container, machine, self._packages)
+
+
+CLEFOS_LIST = [
+    ClefOS('clefos:7', machines=['s390x'], pkg_manager=package_manager.YUM(), packages=[['python3-pip', 'libstdc++', 'glib2', 'libX11', 'libXext', 'libXrender', 'mesa-libGL', 'libICE', 'libSM']]),
+]
