b9374af8b7b21c8a07897ae239c42a0478be9ba7
Carl George <carl@george.computer>
2022-02-11 22:21:39 -0600

    Update CentOS Stream 9 info and container image
    CentOS Stream is not a rolling distribution.  It was originally
    advertised as one to describe how it rolls between minor versions.
    However, it still has major versions and EOL dates, which by the common
    definition makes it not a rolling distribution.  Due the confusion, the
    project stopping using that term to describe the distribution.
    
    The EOL date for CentOS Stream 9 will match the date of the end of the
    RHEL 9 "Full Support" phase, which will be 5 years after the RHEL 9
    launch, which will be approximately 3 years after the RHEL 8 launch.
    
    This also switches the container image from the stream9-development tag
    to the stream9 tag, which includes the ppc64le and s390x architectures.
    
    https://en.wikipedia.org/wiki/Rolling_release
    https://centos.org/centos-stream/
    https://access.redhat.com/support/policy/updates/errata#Full_Support_Phase

--- pep600_compliance/images/os/centos.py
+++ pep600_compliance/images/os/centos.py
@@ -31,9 +31,9 @@ CENTOS_YUM_OLD = [
 
 CENTOS_LIST: list[base.Base] = [
     CentOS(
-        "quay.io/centos/centos:stream9-development",
-        "rolling",
-        machines=["x86_64", "aarch64"],  # TODO "ppc64le", "s390x"
+        "quay.io/centos/centos:stream9",
+        ("EOL:~2027-05-31",),
+        machines=["x86_64", "aarch64", "ppc64le", "s390x"],
         pkg_manager=package_manager.DNF(),
         packages=[
             [
